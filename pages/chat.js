d30ab40b51d07ac01dc870c743c512333909abeb
Daniel <tawata.daniel@gmail.com>
2022-06-13 22:25:46 -0700

    Removed 3000 port

--- pages/chat.js
+++ pages/chat.js
@@ -36,7 +36,7 @@ const Chat = (props) => {
   const notificationsRef = useRef([]);
 
   useEffect(() => {
-    const newSocket = io('ec2-3-83-111-216.compute-1.amazonaws.com:3000', { autoConnect: false });
+    const newSocket = io('ec2-3-83-111-216.compute-1.amazonaws.com', { autoConnect: false });
     setSocket(newSocket);
   }, [])
 
