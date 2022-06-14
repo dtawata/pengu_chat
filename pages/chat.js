85549ecd9aba92f25843ee6535be56658916862e
Daniel <tawata.daniel@gmail.com>
2022-06-13 22:20:10 -0700

    Changed mysql auth

--- pages/chat.js
+++ pages/chat.js
@@ -36,7 +36,7 @@ const Chat = (props) => {
   const notificationsRef = useRef([]);
 
   useEffect(() => {
-    const newSocket = io('http://localhost:3001', { autoConnect: false });
+    const newSocket = io('ec2-3-83-111-216.compute-1.amazonaws.com:3000', { autoConnect: false });
     setSocket(newSocket);
   }, [])
 
